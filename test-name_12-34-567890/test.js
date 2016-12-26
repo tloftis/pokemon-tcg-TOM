51db44bf501fc55394ad10b0a15e246d84e4b944
Tim Loftis <tloftis@redviking.com>
2016-12-26 03:28:52 -0500

    Wasn't suppost to add those

--- test-name_12-34-567890/test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-    xml2js = require('xml2js');
- 
-var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/swiss.tdf', function(err, data) {
-    parser.parseString(data, function (err, result) {
-        console.log(JSON.stringify(result, null, 2));
-    });
-});
